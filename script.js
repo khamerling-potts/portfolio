2085c05f4d269e90be1f258ed27576f307729322
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-07-14 15:56:29 -0400

    added all projects content, wrote JS function to add descriptions to each project

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const readmore1 = document.getElementById('readmore1');
-readmore1.addEventListener("click", function(){
-    const project1 = document.getElementsByClassName('project1')[0];
-    const description = document.createElement('p');
-    description.innerHTML = "<a class='projectLink' href=https://khamerling-potts.github.io/08-todo-javascript/>Open 'To Do List' app</a> <br><br>This project utilizes Javascript to create an interactive to do list. The user can add items, delete items, and mark items as complete. This project was also a great exercise in interactive CSS styling."
-    project1.appendChild(description);
-    readmore1.style.display = "none";
+const readButtons = document.querySelectorAll('.readmore');
+readButtons.forEach((button) => {
+    button.addEventListener("click", showDescription);
 });
+
+function showDescription(event){
+    const elementClicked = event.target;
+    const project = document.getElementById(elementClicked.id.replace("readmore", "project"));
+    const description = document.createElement('p');
+
+    if (project.id === "project1"){
+        description.innerHTML = "<a class='projectLink' href=https://khamerling-potts.github.io/08-todo-javascript/>Open 'To Do List' app</a> <br><br>This project utilizes JavaScript to create an interactive to do list. The user can add items, delete items, and mark items as complete. This project was also a great exercise in interactive CSS styling."
+    }
+    else if (project.id === "project2"){
+        description.innerHTML = "<a class='projectLink' href=https://khamerling-potts.github.io/06-brand-redesign/>Open 'Craigslist Redesign' page</a> <br><br>This page is a non-interactive mock-up of a potential new design for the Craigslist website, intended to be more visually appealing and intuitive."
+    }
+    else if (project.id === "project3"){
+        description.innerHTML = "<a class='projectLink' href=https://khamerling-potts.github.io/07-image-gallery/>Open 'Image Gallery' app</a> <br><br>This interactive image gallery allows users to expand images and navigate through the gallery one image at a time. This was another exercise to practice JavaScript, HTML and CSS."
+    }
+    else if (project.id === "project4"){
+        description.innerHTML = "<a class='projectLink' href=https://khamerling-potts.github.io/05-instagram-clone/>Open 'Instagram Clone' page</a> <br><br>This project uses HTML and CSS to imitate the appearance of the Instagram website."
+    }
+    
+    project.appendChild(description);
+    elementClicked.style.display = "none";
+}
 
