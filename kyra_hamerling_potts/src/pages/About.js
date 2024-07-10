e5c28a3eaad5faf89e6e09b98b3ff797849cf3f4
Kyra <khamerling-potts@wustl.edu>
2024-07-10 16:05:14 -0400

    redoing flex layouts for home and about

--- kyra_hamerling_potts/src/pages/About.js
+++ kyra_hamerling_potts/src/pages/About.js
@@ -2,39 +2,61 @@ import NavBar from "../components/NavBar";
 
 function About() {
   return (
-    <body>
-      <h2 className="sectionHeader">About me</h2>
-      <div class="content">
-        <div class="innerContainer">
-          <section class="aboutInfo">
-            <section class="aboutContainerOuter">
-              <section class="aboutContainerInner">
-                <img src="./headshot2.png" id="headshot" />
-                <p id="aboutText">
-                  I was born in Washington, DC, and have had an interest in STEM
-                  fields and social sciences from a young age. I studied
-                  psychology and computer science at Washington University in
-                  St. Louis, where I was exposed to multiple coding languages,
-                  frameworks, and projects. I spent much of my time there
-                  conducting my own psychological research on emotion regulation
-                  and interpersonal attachment dynamics. Following graduation, I
-                  moved to New York and served as a clinical research
-                  coordinator for suicide prevention at a VA hospital. Although
-                  I will always have an interest clinical psychology, I am
-                  transitioning to software engineering to pursue programming as
-                  another passion of mine. I love the problem-solving and
-                  creativity involved in coding and am excited to continue
-                  learning full stack development at Flatiron School. In my free
-                  time, I enjoy playing piano, flipping furniture, and
-                  discovering new recipes. As I develop my portfolio, I will
-                  continue to add new projects to my website.
-                </p>
-              </section>
-            </section>
-          </section>
-        </div>
-      </div>
-    </body>
+    <div className="content">
+      <h2 className="sectionHeader">About Me</h2>
+      <section class="textContainerInner">
+        <img src="../images/headshot2.png" id="headshot" />
+        <p id="aboutText">
+          I was born in Washington, DC, and have had an interest in STEM fields
+          and social sciences from a young age. I studied psychology and
+          computer science at Washington University in St. Louis, where I was
+          exposed to multiple coding languages, frameworks, and projects. I
+          spent much of my time there conducting my own psychological research
+          on emotion regulation and interpersonal attachment dynamics. Following
+          graduation, I moved to New York and served as a clinical research
+          coordinator for suicide prevention at a VA hospital. Although I will
+          always have an interest clinical psychology, I am transitioning to
+          software engineering to pursue programming as another passion of mine.
+          I love the problem-solving and creativity involved in coding and am
+          excited to continue learning full stack development at Flatiron
+          School. In my free time, I enjoy playing piano, flipping furniture,
+          and discovering new recipes. As I develop my portfolio, I will
+          continue to add new projects to my website.
+        </p>
+      </section>
+    </div>
+    // <>
+    //   <h2 className="sectionHeader">About me</h2>
+    //   <div class="content">
+    //     {/* <div class="innerContainer">
+    //       <section class="aboutInfo">
+    //         <section class="aboutContainerOuter"> */}
+    //     <section class="aboutContainerInner">
+    //       <img src="./headshot2.png" id="headshot" />
+    //       <p id="aboutText">
+    //         I was born in Washington, DC, and have had an interest in STEM
+    //         fields and social sciences from a young age. I studied psychology
+    //         and computer science at Washington University in St. Louis, where I
+    //         was exposed to multiple coding languages, frameworks, and projects.
+    //         I spent much of my time there conducting my own psychological
+    //         research on emotion regulation and interpersonal attachment
+    //         dynamics. Following graduation, I moved to New York and served as a
+    //         clinical research coordinator for suicide prevention at a VA
+    //         hospital. Although I will always have an interest clinical
+    //         psychology, I am transitioning to software engineering to pursue
+    //         programming as another passion of mine. I love the problem-solving
+    //         and creativity involved in coding and am excited to continue
+    //         learning full stack development at Flatiron School. In my free time,
+    //         I enjoy playing piano, flipping furniture, and discovering new
+    //         recipes. As I develop my portfolio, I will continue to add new
+    //         projects to my website.
+    //       </p>
+    //     </section>
+    //     {/* </section>
+    //       </section>
+    //     </div> */}
+    //   </div>
+    // </>
   );
 }
 
--- kyra_hamerling_potts/src/pages/Home.js
+++ kyra_hamerling_potts/src/pages/Home.js
@@ -1,26 +1,15 @@
 import NavBar from "../components/NavBar";
 function Home() {
   return (
-    // <body>
-    <>
-      <div className="content">
-        {/* <div className="innerContainer">
-          <section class="textSection">
-            <section class="textContainer"> */}
-        <h2 className="sectionHeader">Welcome to my website</h2>
-
-        <section class="textContainerInner">
-          <p id="homeIntro1">Hi, I'm Kyra Hamerling-Potts</p>
-          <p id="homeIntro2">
-            A budding, passionate coder transitioning to software engineering.
-          </p>
-        </section>
-        {/* </section>
-          </section>
-        </div> */}
-      </div>
-    </>
-    // </body>
+    <div className="content">
+      <h2 className="sectionHeader">Welcome to my website</h2>
+      <section class="textContainerInner">
+        <p id="homeIntro1">Hi, I'm Kyra Hamerling-Potts</p>
+        <p id="homeIntro2">
+          A budding, passionate coder transitioning to software engineering.
+        </p>
+      </section>
+    </div>
   );
 }
 
