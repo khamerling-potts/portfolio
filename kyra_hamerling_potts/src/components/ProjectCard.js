519a77626f7011b8e02d29892e5252bec09e6a16
Kyra <khamerling-potts@wustl.edu>
2024-07-18 10:56:10 -0400

    was able to overwrite react boostrap accordion colors

--- kyra_hamerling_potts/src/components/ProjectCard.js
+++ kyra_hamerling_potts/src/components/ProjectCard.js
@@ -9,16 +9,16 @@ function ProjectCard({ title, img, github, description }) {
         <Card.Img variant="top" src={img} className="projectImg" />
         <Card.Body>
           <Card.Title className="projHeader">{title}</Card.Title>
-          <Card.Text>
-            <Accordion flush id="projectAccordion">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header className="custom-accordion-header">
-                  Read More
-                </Accordion.Header>
-                <Accordion.Body>{description}</Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </Card.Text>
+          {/* <Card.Text> */}
+          <Accordion flush id="projectAccordion">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header className="custom-accordion-header">
+                Read More
+              </Accordion.Header>
+              <Accordion.Body>{description}</Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+          {/* </Card.Text> */}
         </Card.Body>
       </Card>
     </Col>
